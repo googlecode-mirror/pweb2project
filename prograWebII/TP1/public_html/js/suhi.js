88ef7856bd2557f5d9f7c28c95e9acd8a6fb96b7
alessandrelloire <alessandrelloire@gmail.com>
2014-09-15 02:30:38 +0000

    irene.alessandrello

--- prograWebII/TP1/public_html/js/suhi.js
+++ prograWebII/TP1/public_html/js/suhi.js
@@ -1,17 +1,17 @@
-
-window.onload = function() {
-alert("Hola Javascript!");
-}
-
-
-$(window).load(function() {
-	alert("Hola JQuery load!");
-});
-/*
-$(document).ready(function(){
-   alert("Hola JQuery ready!");
-}); 
-*/
+//
+//window.onload = function() {
+//alert("Hola Javascript!");
+//}
+//
+//
+//$(window).load(function() {
+//	alert("Hola JQuery load!");
+//});
+///*
+//$(document).ready(function(){
+//   alert("Hola JQuery ready!");
+//}); 
+//*/
 
 $(function() {
 $( "#tabs" ).tabs();
