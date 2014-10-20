7bed3b26077609c91f4e681f32f7cfddfa5152bd
alessandrelloire <alessandrelloire@gmail.com>
2014-10-20 22:20:29 +0000

    funcionamiento de datepicker - marcelo

--- prograWebII/TPFinal/development/formulario_de_vuelo.php
+++ prograWebII/TPFinal/development/formulario_de_vuelo.php
@@ -7,8 +7,8 @@ require('calendario.php');
 <head>
 	<title>Reserva de Vuelos</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<script type="text/javascript" src="javascript/jquery-1.3.1.min.js"></script>
-	<script type="text/javascript" src="javascript/jquery.functions.js"></script>
+	<script language="javascript" type="text/javascript" src="javascript/jquery-1.3.1.min.js"></script>
+	<script language="javascript" type="text/javascript" src="javascript/jquery.functions.js"></script>
 
 </head>
 <body>
@@ -42,13 +42,13 @@ require('calendario.php');
 				echo "<option value = ".$res[$i][1]." >".$res[$i][1]." - ".$res[$i][0]."</option>";}*/
 			?>
 			</select><br/><br/>
-	Fecha ida:<input type="text" name="fecha" id="fecha"  /> <a onclick="show_calendar()" style="cursor: pointer;"><small>(Seleccionar)</small></a>
+	Fecha ida:<input type="text" name="fecha" id="fecha"  /> <a onclick="show_calendar('calendario');" style="cursor: pointer;"><small>(Seleccionar)</small></a>
     <div id="calendario">
-    <?php calendar_html() ?>
+    <?php calendar_html("calendario","fecha"); ?>
     </div><br/><br/>
-	Fecha Vuelta: <input type="text" name="fecha" id="fecha"  /> <a onclick="show_calendar()" style="cursor: pointer;"><small>(Seleccionar)</small></a>
-    <div id="calendario">
-    <?php calendar_html() ?>
+	Fecha Vuelta: <input type="text" name="fechaV" id="fechaV"  /> <a onclick="show_calendar('calendarioV');" style="cursor: pointer;"><small>(Seleccionar)</small></a>
+    <div id="calendarioV">
+    <?php calendar_html("calendarioV","fechaV"); ?>
     </div><br/><br/>
 	Clase: <select  id="id_clase" name="id_clase" >
 				<option value="Economy">Economy</option>
