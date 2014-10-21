59a6f3cd37bc1ddf77d3e13ec7076090ab5888cf
mhconde.mc <mhconde.MC@gmail.com>
2014-10-21 15:37:38 +0000

    Validaciones del formulario de vuelo

---
+++ prograWebII/TPFinal/javascript/Validaciones.js
@@ -0,0 +1,29 @@
+function validarForm(){
+	var err = '';
+	var fecha = document.getElementById("fecha");
+	var origen = document.getElementById("id_origen");
+	var destino = document.getElementById("id_destino");
+	var fechaV = document.getElementById("fechaV");
+	var clase = document.getElementById("id_clase");
+	
+	if (fecha.value == ""){
+		err += "Debe seleccionar una fecha de ida \n";
+		}
+	if (origen.value == ""){
+			err += "Debe seleccionar un origen \n";
+			}
+	if (destino.value == ""){
+			err += "Debe seleccionar un destino \n";
+			}
+	if (fechaV.value == ""){
+			err += "Debe seleccionar una fecha de vuelta \n";
+			}		
+	if (clase.value == ""){
+		err += "Debe seleccionar una clase \n";
+		}	
+	if ( err == "" ){
+		document.form_vuelo.submit();
+	}else{
+		alert(err);
+  }
+  }
