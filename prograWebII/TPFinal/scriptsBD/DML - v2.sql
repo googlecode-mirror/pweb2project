cb8a73d33dfd1e337cb8da6ca506583805b0a031
alessandrelloire <alessandrelloire@gmail.com>
2014-10-27 00:48:56 +0000

    26/10/2014 irene.alessandrello Nuevas consultas

--- prograWebII/TPFinal/scriptsBD/DML - v2.sql
+++ prograWebII/TPFinal/scriptsBD/DML - v2.sql
@@ -5,6 +5,11 @@ select * from avion;
 -- consulta combo origen y destino
 select c_cod, c_desc from ciudad;
 -- ----------------------------------------------------------
+-- consulta en cronograma si hay vuelos segun parametros ingresados
+select * from cronograma 
+where c_origen = "SAAC";
+-- and ...
+-- and ...
 -- ----------------------------------------------------------
 -- ----------------------------------------------------------
 -- ----------------------------------------------------------
