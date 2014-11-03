ff5e37dcc445dee4ae6414e7b78c3b9d8513f037
alessandrelloire <alessandrelloire@gmail.com>
2014-11-03 03:46:47 +0000

    irene.alessandrello - Se descomenta la validacion de parametro ciudad de destino ya que se agrego la logica para que el combo desplegable de la pantalla de busqueda de vuelos entregue este dato.

--- prograWebII/TPFinal/javascript/formulario_de_vuelo.js
+++ prograWebII/TPFinal/javascript/formulario_de_vuelo.js
@@ -1,3 +1,48 @@
-$(window).load(function(){
-	//alert("La pagina esta cargada...")
-	});
+$(document).ready(function(){
+	
+	$("#boton_buscar_id").click(function() {
+		alert("click!");
+		//se validan datos ingresados
+		var err = '';
+		var fecha = document.getElementById("fecha");
+		var origen = document.getElementById("id_origen");
+		var destino = document.getElementById("id_destino");
+		var fechaV = document.getElementById("fechaV");
+		var clase = document.getElementById("id_clase");
+		
+		//TEMP Comento las validaciones para probar submit
+//		if (fecha.value == ""){
+//			err += "Debe seleccionar una fecha de ida \n";
+//		}
+//		if	((fecha.value > fechaV.value)||(fecha.value == fechaV.value)){
+//			err += "Debe seleccionar una rango de fechas correcto\n";
+//		}
+		if (origen.value == ""){
+				err += "Debe seleccionar un origen \n";
+		}
+		else{
+			alert("origen.value: "+origen.value);
+		}
+		
+		if (destino.value == ""){
+				err += "Debe seleccionar un destino \n";
+		}
+		else{
+			alert("destino.value: "+destino.value);
+		}
+//		if (fechaV.value == ""){
+//				err += "Debe seleccionar una fecha de vuelta \n";
+//		}		
+//		if (clase.value == ""){
+//			err += "Debe seleccionar una clase \n";
+//		}
+		
+		if ( err == "" ){
+			document.form_vuelo.submit();
+			//si se valido ok los datos ingresados envio el formulario	
+			$("#form_vuelo_id").submit();		
+		}else{
+			alert(err);
+	    }
+ })
+});
